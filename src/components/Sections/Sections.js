bbf831b0b530cea2989bd3b7858f76813178220d
hipopridwan <ridwanhipop@gmail.com>
2023-03-15 00:24:15 +0600

    daynamic all data show

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./Sections.css";
+import carInfos from "../../carInfo.json";
 
 const Sections = () => {
   return (
-    <div className="wrap">
-      <div className="item-text">
-        <h1>Model 3</h1>
-        <p>Leasing starting at $349/mo</p>
-      </div>
-      <div className="items-btn">
-        <div className="group-btn">
-          <button className="left-btn btn">Custom Order</button>
-          <button className="right-btn">Demo Drive</button>
-        </div>
-        <div className="btn-arrow">
-          <img src="/images/down-arrow.svg" alt="" />
-        </div>
-      </div>
-    </div>
+    <>
+      {carInfos.map((carInfo) => {
+        return (
+          <div className="wrap" key={carInfo.id}>
+            <div className="con-img">
+              <img src={carInfo.image} alt="" />
+            </div>
+            <div className="item-text">
+              <h1>${carInfo.title}</h1>
+              <p>${carInfo.description}</p>
+            </div>
+            <div className="items-btn">
+              <div className="group-btn">
+                <button className="left-btn btn">Custom Order</button>
+                <button className="right-btn btn">Demo Drive</button>
+              </div>
+              <div className="btn-arrow">
+                <img src="/images/down-arrow.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
